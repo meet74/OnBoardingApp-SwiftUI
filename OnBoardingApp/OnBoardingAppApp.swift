93371a83e6b3d02797c04bfcff7868b3fa1503aa
meet74 <meet237@gmail.com>
2023-11-13 17:55:47 -0500

    Initial Commit

---
+++ OnBoardingApp/OnBoardingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingAppApp.swift
+//  OnBoardingApp
+//
+//  Created by Meet Patel on 2023-11-13.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
